cac843b3538617b082e1932696a1910630b0ccec
zqdfound <15950515069@163.com>
2019-01-05 19:41:17 +0800

    分布式

---
+++ tensquare_common/src/main/java/entity/PageResult.java
@@ -0,0 +1,36 @@
+package entity;
+
+import java.util.List;
+
+/**
+ * 分页结果类
+ * @param <T>
+ */
+public class PageResult <T>{
+    private long total;
+    private List<T> rows;
+
+    public PageResult() {
+    }
+
+    public PageResult(long total, List<T> rows) {
+        this.total = total;
+        this.rows = rows;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public List<T> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<T> rows) {
+        this.rows = rows;
+    }
+}
