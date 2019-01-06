1aafd1f278b4d139a68b5f463e12bb9afbf9c299
zqdfound <15950515069@163.com>
2019-01-06 20:17:27 +0800

    2019/1/06

---
+++ tensquare_article/src/main/java/com/tensquare/article/dao/ArticleDao.java
@@ -0,0 +1,32 @@
+package com.tensquare.article.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.tensquare.article.pojo.Article;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * 数据访问接口
+ * @author Administrator
+ *
+ */
+public interface ArticleDao extends JpaRepository<Article,String>,JpaSpecificationExecutor<Article>{
+    /**
+     * 审核
+     * @param id
+     */
+    @Modifying//增删改
+    @Query("update Article set state='1' where id=?1")
+    public void examine(String id);
+
+    /**
+     * 点赞
+     * @param id
+     * @return
+     */
+    @Modifying
+    @Query("update Article a set thumbup=thumbup+1 where id=?1")
+    public int updateThumbup(String id);
+}
